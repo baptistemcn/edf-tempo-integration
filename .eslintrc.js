c3f14514fc61a37d458310dd3f7d98198c6d3923
Baptiste Marcon <baptistemcn@icloud.com>
2024-12-31 21:14:46 +0100

    chore(lint): update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,20 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    quotes: [
+      'error',
+      'double',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true,
+      },
+    ],
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: false,
+        semi: true,
+      },
+    ],
   },
 };
