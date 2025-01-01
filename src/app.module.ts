621588dabe776a9a7b4cdae6c64eb635eb6df98e
Baptiste Marcon <baptistemcn@icloud.com>
2025-01-01 19:45:48 +0100

    chore(shared): add http module & throttler module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from "@nestjs/common";
+import { APP_GUARD } from "@nestjs/core";
+import { ThrottlerGuard } from "@nestjs/throttler";
 
 import { TempoModule } from "./tempo/tempo.module";
+import { SharedModule } from "./shared/shared.module";
 
 @Module({
-  imports: [TempoModule],
+  imports: [TempoModule, SharedModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
